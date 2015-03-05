bf57136261b1ab0a70d38e57914f8378fae5feb8
karan sharma <karan.sharma516@yahoo.com>
2015-03-05 11:46:52 -0800

    cleaned up project

---
+++ COMP2068-MailPilotDemo/Scripts/game.ts
@@ -0,0 +1,29 @@
+﻿var canvas;
+var stage: createjs.Stage;
+
+// Game Objects 
+
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+   
+
+    stage.update(); // Refreshes our stage
+}
+
+
+
+// Our Game Kicks off in here
+function main() {
+    
+    
+}
