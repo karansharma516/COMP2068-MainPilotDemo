802689042f5c2a336dc64bb873b23608caab9105
karan sharma <karan.sharma516@yahoo.com>
2015-03-05 12:00:10 -0800

    Added Preloader

--- COMP2068-MailPilotDemo/Scripts/game.ts
+++ COMP2068-MailPilotDemo/Scripts/game.ts
@@ -1,8 +1,23 @@
 ﻿var canvas;
 var stage: createjs.Stage;
+var assetLoader: createjs.LoadQueue;
 
+var manifest = [
+    { id: "cloud", src: "assets/images/cloud.png" },
+    { id: "island", src: "assets/images/island.png" },
+    { id: "ocean", src: "assets/images/ocean.png" },
+    { id: "plane", src: "assets/images/plane.png" }
+];
 // Game Objects 
 
+function preload() {
+
+    var assetLoader = new createjs.LoadQueue();
+    assetLoader.installPlugin(createjs.Sound);
+    assetLoader.on("complete", init, this);
+    assetLoader.loadManifest(manifest);
+
+}
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -25,5 +40,5 @@ function gameLoop() {
 // Our Game Kicks off in here
 function main() {
     
-    
+    console.log("Getting the game started");
 }
